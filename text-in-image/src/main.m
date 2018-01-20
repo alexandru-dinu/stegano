5bbf1e1a0434ec8200a49fb2f546cc91a6a7eff5
alexandru-dinu <alex.dinu07@gmail.com>
2018-01-20 12:02:42 +0200

    fix some typos and prepare demo

--- text-in-image/src/main.m
+++ text-in-image/src/main.m
@@ -1,11 +1,17 @@
 clear; clc;
 
-input_image = '../img/lena-big.png';
-output_image = '../img/emb_lena.png';
+
+% input_image = '../img/lena-big.png';
+% output_image = '../img/stegano.png';
+% input_text = '../text/lorem-ipsum.txt';
+
+input_image = '../img/animal_farm.png';
+output_image = '../img/stegano.png';
 input_text = '../text/animal-farm.txt';
 
+
 tic
-%embed(input_image, input_text, output_image);
+embed(input_image, input_text, output_image);
 toc
 
 tic
@@ -13,6 +19,6 @@ text = extract(output_image);
 toc
 
 % save text to file
-fid = fopen('../text/out_animal-farm.txt', 'wt');
+fid = fopen('../text/out_text.txt', 'wt');
 fwrite(fid, text(1:end))
 fclose(fid);
